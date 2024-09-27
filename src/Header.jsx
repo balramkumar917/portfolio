3572e82cbf1932a9ce2fce765e5aa8aceaa2d8e2
Balram <balramkumar917@gmail.com>
2024-09-27 10:25:47 +0530

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,10 +4,10 @@ import { Education } from './Education'
 export const Header = ( props) => {
   return (
     <div className='header'>
-        <a href=""><p>HOME</p></a>
-        <a href=""><p>SKILLS</p></a>
-        <a href=""><p>PROJECTS</p></a>
-        <a href=""><p>EDUCATION</p></a>
+        <p onClick={""}>HOME</p>
+        <p onClick={""}>SKILLS</p>
+        <p onClick={""}>PROJECTS</p>
+        <p onClick={""}>EDUCATION</p>
     </div>
   )
 }
