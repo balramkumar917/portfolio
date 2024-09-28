72960371aea5710acd403e27fb1f3d3407d6e72b
Balram <balramkumar917@gmail.com>
2024-09-28 16:48:22 +0530

    update

--- src/Project.jsx
+++ src/Project.jsx
@@ -9,9 +9,9 @@ export const Project = () => {
     <div className='project'>
         <h3>Portfolio</h3>
         <p>
-          Languages Used - JavaScript, HTML, CSS<br/>
-            Framework used- Vite + React<br />
-            Code editor - VS Code 
+          Languages Used : JavaScript, HTML, CSS<br/>
+            Framework used : Vite + React<br />
+            Code editor : VS Code 
         </p>
         <div className='project-link'>
           <a href="https://github.com/balramkumar917/portfolio">Github Link</a>
@@ -19,17 +19,17 @@ export const Project = () => {
     </div>
     <div className='project'>
         <h3>Online Attendance System</h3>
-        <p>Languages - HTML, CSS, Javascript, PHP, Xampp, VS Code.<br/>
-            Databases - MySQL<br/>
-            Code editor - VS Code</p>
+        <p>Languages : HTML, CSS, Javascript, PHP, Xampp, VS Code.<br/>
+            Databases : MySQL<br/>
+            Code editor : VS Code</p>
     </div>
     <div className='project'>
         <h3>Pocket Certificate Using Double Encryption</h3>
-        <p>Languages Used - Python<br/>
-            Framework used- Kivy Framework <br />
-            Security - AES DES Encryption <br />
-            Code editor - VS Code <br />
-            Database - MySQL</p>
+        <p>Languages Used : Python<br/>
+            Framework used : Kivy Framework <br />
+            Security : AES DES Encryption <br />
+            Code editor : VS Code <br />
+            Database : MySQL</p>
     </div>
     
 
